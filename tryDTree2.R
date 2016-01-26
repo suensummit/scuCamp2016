e0459a2cb3299141c1e7a74302673a85b2e076bb
Summit Suen <summit.suen@gmail.com>
2016-01-27 00:02:36 +0800

    git init

---
+++ tryDTree2.R
@@ -0,0 +1,16 @@
+library(rpart)
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
+
+plot(fit)
+text(fit)
+
+install.packages('RGtk2')
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+fancyRpartPlot(fit)
